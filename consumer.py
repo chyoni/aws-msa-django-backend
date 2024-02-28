ff93e193aaf2c1c6e7686466ad0bc8ae05b61471
chyonee <chiwon99881@gmail.com>
2024-02-28 16:10:24 +0900

    communication via rabbitmq

--- consumer.py
+++ consumer.py
@@ -1,24 +1,52 @@
 import pika
+import json
+
+# startapp 명령어로 만든 파일이 아닌 곳에서 Django에 등록된 모델이나 기능을 사용하려면 이렇게 환경 설정을 해줘야 한다.
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import django
+
+django.setup()
+
 from config.constants import RABBITMQ_URL
+from order.models import Order
 
+# pika로 RabbitMQ 연결
 params = pika.URLParameters(RABBITMQ_URL)
-
+# Channel을 만들기 위한 RabbitMQ와의 Connection 생성
 connection = pika.BlockingConnection(params)
-
+# Connection의 채널을 생성
 channel = connection.channel()
 
+# 'order' 라는 이름으로 된 큐를 선언
 channel.queue_declare(queue='order')
 
+
 def callback(ch, method, properties, body):
+    print('---------- Received in order ----------')
 
-    print('Received in order')
+    if properties.content_type == 'order_delivery_finished':
+        id = json.loads(body)
 
-    print(body)
+        print(id)
 
+        order = Order.objects.get(id=id)
+
+        order.delivery_finish = True
+
+        order.save()
+
+        print('order delivery finished')
+
+
+# 'order'로 만들어진 큐에 들어온 메시지를 구독 on_message_callback 으로 메시지가 들어올 때 처리할 callback 함수를 정의
 channel.basic_consume(queue='order', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
 
+# 채널 구독
 channel.start_consuming()
 
+# 채널 종료
 channel.close()
--- order/producer.py
+++ order/producer.py
@@ -2,15 +2,21 @@ import pika
 import json
 from config.constants import RABBITMQ_URL
 
+# pika로 RabbitMQ 연결
 params = pika.URLParameters(RABBITMQ_URL)
-
+# Channel을 만들기 위한 RabbitMQ와의 Connection 생성
 connection = pika.BlockingConnection(params)
-
+# Connection의 채널을 생성
 channel = connection.channel()
 
+
+# 채널에 전송할 메시지를 만들어내는 함수 method는 식별자라고 생각하면 된다.
+# 예를 들어, Shop을 만들어내는 API를 호출하고 그 API에서 이 publish()를 호출할 때 'shop_created'라는 식별자를 던져줘서 어떤 식별자인지 인식한다.
+# body는 넘겨받을 데이터. 예를 들어, Shop을 만들어내는 API라면 Shop을 만들기 위해 필요한 데이터들이 넘어올 것
 def publish(method, body):
 
     properties = pika.BasicProperties(method)
 
+    # 큐가 'owner'로 된 곳으로 메시지를 보낸다.
     channel.basic_publish(exchange='', routing_key='owner', body=json.dumps(body), properties=properties)
 
